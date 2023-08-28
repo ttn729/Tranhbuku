dc58eb26fe1ac26cd84b25046bc78cad77b6b5b4
Thomas Nguyen <ttn@ou.edu>
2023-08-28 18:43:03 -0500

    Implement timer

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,19 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+async function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function startGameTimer() {
+  for (let i = 60; i >= 0; i--) {
+    console.log(i);
+    io.emit('set timer', 'Time: ' + i);
+
+    await delay(1000); // Wait for 1 second
+  }
+}
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -19,14 +32,14 @@ io.on('connection', (socket) => {
   socket.on('join', (username) => {
     usernameMap[socket.id] = username;
     io.emit('chat message', username + ' has entered the chat.', 'SYSTEM');
-    io.emit('user join', Object.values(usernameMap));
+    io.emit('update users', Object.values(usernameMap));
   });
   
   socket.on('disconnect', () => {
     console.log('user ' + socket.id  + ' has disconnected');
     io.emit('chat message', usernameMap[socket.id] + ' has disconnected.', 'SYSTEM');
     delete usernameMap[socket.id];
-    io.emit('user join', Object.values(usernameMap));
+    io.emit('update users', Object.values(usernameMap));
   });
 
   socket.on('chat message', (msg, username) => {
@@ -34,7 +47,17 @@ io.on('connection', (socket) => {
     console.log('message: ' + msg + ' username: ' + username);
     io.emit('chat message', msg, username);
   });
+
+  socket.on('start game', () => {
+    console.log('start game')
+    startGameTimer();
+  });
+
+
+
 });
+
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
