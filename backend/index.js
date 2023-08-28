a68a1843b8cb5b4a04cda97e8db53af11063b724
Thomas Nguyen <ttn@ou.edu>
2023-08-28 15:53:25 -0500

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+let usernameMap = {}
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('join', (username) => {
+    usernameMap[socket.id] = username;
+    io.emit('chat message', username + ' has entered the chat.', 'SYSTEM');
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user ' + socket.id  + ' has disconnected');
+    io.emit('chat message', usernameMap[socket.id] + ' has disconnected.', 'SYSTEM');
+  });
+
+  socket.on('chat message', (msg, username) => {
+    console.log(typeof username)
+    console.log('message: ' + msg + ' username: ' + username);
+    io.emit('chat message', msg, username);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
